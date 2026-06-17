798ebf2f592432b436e7d059298920eb03fd01de
Sparsh Singhal <241079331+singhalsparsh@users.noreply.github.com>
2026-06-17 14:03:33 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
     ],
   },
   turbopack: {},
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
