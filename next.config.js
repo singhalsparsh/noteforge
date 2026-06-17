505ee8b6c5fcb83b63aeb80a5223e2ad8bc08392
Sparsh Singhal <241079331+singhalsparsh@users.noreply.github.com>
2026-06-17 14:09:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' },
     ],
   },
-  turbopack: {},
   eslint: {
     ignoreDuringBuilds: true,
   },
